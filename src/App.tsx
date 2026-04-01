39def77e770a9c4c0b0ec461f37e38c73d99c404
Thenujana <chenarathenujana@gmail.com>
2026-04-01 20:41:22 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+
+  return (
+     <><Home /><>
+      <h1>Hello This is the start</h1>
+    </></>
+   
+  )
+}
+
+export default App
